a65b21fa95aa0acc5f51c8e4bb34aea5e8fdb159
gmccoy42 <gmccoy4242@gmail.com>
2015-08-05 19:31:54 -0600

    Ported to work on wrapupnews.org

--- wrapupnews/settings.py
+++ wrapupnews/settings.py
@@ -60,7 +60,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/gmccoy/Programming/wrapupnews/wrapup/templates/wrapup/",
+    "/home2/wrapupne/wrapupnews/wrapupsite/wrapup/templates/wrapup/",
 )
 
 WSGI_APPLICATION = 'wrapupnews.wsgi.application'
@@ -72,9 +72,9 @@ WSGI_APPLICATION = 'wrapupnews.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wrapup_db',
-        'USER': 'root',
-        'PASSWORD': 'Conestoga1',
+        'NAME': 'wrapupne_wrapup_db',
+	'USER': 'wrapupne_root',
+	'PASSWORD': '&!Tempest42S&&',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
@@ -97,5 +97,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = '/home2/wrapupne/www/static/'
+STATIC_URL = '/static/' 
 
-STATIC_URL = '/static/'
