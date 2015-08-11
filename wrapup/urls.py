30c1a5417b9a89b5b41ae9523dda80306d15b8ab
gmccoy42 <gmccoy4242@gmail.com>
2015-08-11 19:55:53 -0400

    Fixed a chrome redirect issue

--- wrapup/urls.py
+++ wrapup/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^.*$', views.index, name='index'),
     url(r'^site/$', views.site, name='site'),
     url(r'^register/$', views.register, name='register'),
     url(r'^key/$', views.key, name='key'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/wrapup/'})
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/wrapup/'}),
+   
 ]
--- wrapupnews/urls.py
+++ wrapupnews/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('wrapup.urls', namespace="wrapup")),
     url(r'^wrapup/', include('wrapup.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
