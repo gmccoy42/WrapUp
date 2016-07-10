90486f3b4fbe8092323b3e3ed6d16e91a40d8da0
Greg McCoy <gmccoy4242@gmail.com>
2016-07-10 11:55:16 -0400

    changing Keys to Key

--- wrapup/admin.py
+++ wrapup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Keys, Site, Stories, Rank
+from .models import Key, Site, Stories, Rank
 
 # Register your models here.
 admin.site.register(Site)
