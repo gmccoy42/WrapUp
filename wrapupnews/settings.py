c6cf3a8a923418c1de3042cf199f7f3af00a551c
Greg McCoy <gregmccoy@gfa.org>
2016-07-10 14:31:24 -0400

    Adding restframework

--- wrapupnews/settings.py
+++ wrapupnews/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'wrapup',
 )
 
@@ -73,7 +74,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'wrapup_db',
-        'USER': 'root',
+        'USER': '',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
